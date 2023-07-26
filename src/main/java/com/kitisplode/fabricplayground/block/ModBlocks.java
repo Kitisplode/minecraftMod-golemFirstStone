37372a1a912b3f7c015b0d11687d2b16677311c7
Kitisplode <kitsucode@ymail.com>
2023-07-26 17:25:20 -0400

    added recipe for the stone head

--- src/main/java/com/kitisplode/fabricplayground/block/ModBlocks.java
+++ src/main/java/com/kitisplode/fabricplayground/block/ModBlocks.java
@@ -6,17 +6,18 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.WearableCarvedPumpkinBlock;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.ConstantIntProvider;
 
 public class ModBlocks
 {
     public static final Block BLOCK_HEAD_STONE = registerBlock("block_head_stone", new BlockHeadStone(FabricBlockSettings.copyOf(Blocks.STONE)));
-    public static final Block BLOCK_CORE_STONE = registerBlock("block_core_stone", new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
+    public static final Block BLOCK_CORE_STONE = registerBlock("block_core_stone", new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE), ConstantIntProvider.create(50)));
 
     private static Block registerBlock(String pName, Block pBlock)
     {
